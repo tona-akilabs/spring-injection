f081c2bd9cf373a88206a60127f87caf25a75fd5
Tona <tona.chheun@akilabs.co>
2025-08-21 08:52:52 +0700

    init

---
+++ src/main/java/com/example/spring_injection/SpringInjectionApplication.java
@@ -0,0 +1,11 @@
+package com.example.spring_injection;
+
+
+
+public class SpringInjectionApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Spring Injection!");
+	}
+
+}
---
+++ src/test/java/com/example/spring_injection/SpringInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_injection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
